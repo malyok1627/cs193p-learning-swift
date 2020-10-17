dc013c095b38015e9ea4e3a56a8b5626297924d8
Yarik Malyok <yaroslav.smalko@protonmail.com>
2020-10-18 00:30:49 +0200

    deleted "content: " value, as it also can be ignored

--- cs193p-learning-swift/ContentView.swift
+++ cs193p-learning-swift/ContentView.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(content: {
-            ForEach(0..<4, content: { index in
-                ZStack(content: {
+        HStack() {
+            ForEach(0..<4) { index in
+                ZStack() {
                     RoundedRectangle(cornerRadius: 10.0)
                         .fill(Color.white)
                     RoundedRectangle(cornerRadius: 10.0)
                         .stroke(lineWidth: 3.0)
                     Text("👻")
-                })
-            })
-        })
+                }
+            }
+        }
         .foregroundColor(Color.orange)
         .padding()
         .font(Font.largeTitle)
