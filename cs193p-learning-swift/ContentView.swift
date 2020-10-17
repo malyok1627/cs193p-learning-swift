48b1a87da1b158c6545b95e7489f848783c5cb9a
Yarik Malyok <yaroslav.smalko@protonmail.com>
2020-10-18 00:29:20 +0200

    deleted "returns", as they can be ignored in swift

--- cs193p-learning-swift/ContentView.swift
+++ cs193p-learning-swift/ContentView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return HStack(content: {
-            return ForEach(0..<4, content: { index in
-                return ZStack(content: {
+        HStack(content: {
+            ForEach(0..<4, content: { index in
+                ZStack(content: {
                     RoundedRectangle(cornerRadius: 10.0)
                         .fill(Color.white)
                     RoundedRectangle(cornerRadius: 10.0)
