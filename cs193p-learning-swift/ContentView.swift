3b32bb09323d194280f64d0f0d82d0fd87a8f004
Yarik Malyok <yaroslav.smalko@protonmail.com>
2020-10-17 23:31:32 +0200

    Initial Commit

---
+++ cs193p-learning-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs193p-learning-swift
+//
+//  Created by Yaroslav Smalko on 17.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs193p-learning-swift/cs193p_learning_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_learning_swiftApp.swift
+//  cs193p-learning-swift
+//
+//  Created by Yaroslav Smalko on 17.10.20.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_learning_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
