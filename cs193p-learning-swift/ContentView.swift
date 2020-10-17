488fea702c8582b56b366be3c4f0cc50854c2874
Yarik Malyok <yaroslav.smalko@protonmail.com>
2020-10-18 00:28:15 +0200

    added some blocks with emojis

--- cs193p-learning-swift/ContentView.swift
+++ cs193p-learning-swift/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack(content: {
+            return ForEach(0..<4, content: { index in
+                return ZStack(content: {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .stroke(lineWidth: 3.0)
+                    Text("👻")
+                })
+            })
+        })
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
     }
 }
 
